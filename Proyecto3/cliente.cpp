d7459a3a8c2a19096e997ec86512be7aa29611b9
RandyVig <rvillanuevag1603@alumno.ipn.mx>
2020-04-17 16:12:21 -0500

    Agregando Cliente: Generador de Coordenadas Limpiando código

--- Proyecto3/cliente.cpp
+++ Proyecto3/cliente.cpp
@@ -1,70 +1,66 @@
 #include "Solicitud.h"
 #include <iostream>
-#include <cmath>
+#include <unistd.h>
 #include <cstring>
 #include <thread>
+#include <cmath>
 #include <mutex>
 
 using namespace std;
 
-const double PI = 3.141516;
-const double tam = PI / 160;
+const double PI = 3.141592653589;
+const double periodos = PI / 160;
+char ip[16]= "127.0.0.1";
 mutex m;
 int pto = 7200;
 
-void getCoords(int * num, int iteracion , int term);
-double sumatoria( double x , int termn);
-int ValorY( double valor );
+void graficar( int i );
+int escala( double y );
+void calcularCoordenadas(int * coordenada, int iteracion , int termino);
+double serieFourier( double x , int termino);
 
-void funcion(int i){
-    int num[2],resultado;
-    char ip[16]= "192.168.100.23";
-    //char ip[16]= "127.0.0.1";
-    Solicitud sol;
-    m.lock();
-    for ( int j = 1; j <= 800 ; j++){
-        getCoords(num,j,i);
-        printf("x: %d Y: %d\n",num[0],num[1]);
-        memcpy(&resultado,sol.doOperation( ip , pto , 1 ,(char*)&num ),4);
-    }
-    m.unlock();
-}
-
-int main(int argc, char* argv[]){
-    for ( int i = 0 ; i <= 20 ; i++ ){
-        thread th1(funcion, i),th2(funcion, i);
-        th1.join();
-        th2.join();
+int main( void ){
+    for ( int i = 0 ; 1 ; i++ ){
+        thread dibujar(graficar, i),borrar(graficar, i);
+        dibujar.join(); borrar.join();
     }
     return 0;
 }
 
-int ValorY( double valor ) {
-    double conver = ((100 * (valor + 1.4)) / 2.8) - 150 ;
-    double coor = 150 - conver;
-    return (int)coor;
+void graficar(int i){
+    int coordenada[2],result;
+    Solicitud sol;
+    m.lock();
+    for ( int j = 1; j <= 800 ; j++){
+        calcularCoordenadas(coordenada,j,i);
+        memcpy(&result,sol.doOperation( ip , pto , 1 ,(char*)&coordenada ),4);
+        usleep(1500);
+    }
+    m.unlock();
 }
 
+int escala( double y ) {
+    double coord = 300 - (40*y) ;
+    return (int)coord;
+}
 
-void getCoords( int * num , int iteracion , int term){
+void calcularCoordenadas( int * coordenada , int iteracion , int termino){
     double x,y;
-    x = ( (PI) + (tam*iteracion) );
-    y= 2*(sin(x)+sumatoria(x,term));
-    int yy=ValorY(y);
-    num[0] = iteracion;
-    num[1] = yy;
+    x = ( (PI) + ( periodos * iteracion ) );
+    y = 2 * ( sin(x) + serieFourier(x,termino) );
+    coordenada[0] = iteracion;
+    coordenada[1] = escala(y);
 }
 
-double sumatoria( double x , int termn){
-    if( termn == 0)
-        return 0.0;
+// Onda en Diente de Sierra
+double serieFourier( double x , int termino){
     double suma = 0.0, parcial, aux = 2;
-    for ( int i = 0; i < termn ; i++){
+    for ( int i = 0; i < termino ; i++){
         parcial = ( 1 / aux ) * sin(aux*x);
-        if (((int)aux%2)==0) 
-            parcial*=-1;  
+        if ( (int)aux % 2 == 0 ) 
+            parcial *= -1;  
         suma += parcial;
-        aux+=1;
+        aux +=1 ;
     }
     return suma;
 }
